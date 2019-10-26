8992db1eaeed31f21a069a12ae8c163e889ef566
Taishawn King <taishawnking1@yahoo.com>
2019-10-26 18:00:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,11 +17,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array.pop
+ remove_element_from_end_of_array= remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array = remove_element_from_start_of_array.
 end
 
 def retrieve_element_from_index(array, index_number)
